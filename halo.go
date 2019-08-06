89da50c52e18f36659e1eaf4ae2bfee8a111eb9e
Administrator <2084267015@qq.com>
2019-08-06 22:39:18 +0800

    go basic

--- halo.go
+++ halo.go
@@ -1,7 +1,19 @@
-package main
+package main //默认的包
 
 import "fmt"
 
+//定义常量
+const NAME = "GO"
+
+//全局var
+var addr = "dd g"
+
+/*
+多行注释
+默认的main方法
+*/
 func main() {
 	fmt.Println("halo")
+	fmt.Print(addr)
+	fmt.Print(NAME)
 }
