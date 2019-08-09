a66d1b38cb3a9122b5d4d94ab86cc71eba419d08
Administrator <2084267015@qq.com>
2019-08-09 20:42:43 +0800

    go basic

--- OperatorUse.go
+++ OperatorUse.go
@@ -14,4 +14,30 @@ func main() {
 	fmt.Println(a * b) //12
 	fmt.Println(a / b) //0
 	fmt.Println(a % b) //3
+	//--关系运算符
+	fmt.Println("--关系运算符")
+	fmt.Println(a == b) //false
+	fmt.Println(a >= b) //false
+	fmt.Println(a <= b) //true
+	fmt.Println(a != b) //true
+	fmt.Println(a > b)  //false
+	fmt.Println(a < b)  //true
+	fmt.Println("--logic运算符")
+	fmt.Println(true && false) //false
+	fmt.Println(true && true)  //true
+	fmt.Println(true || false) //tru
+	fmt.Println(!true)
+	fmt.Println("--按位运算符")
+	//& 都为1-->1 ; | 有一个1-->1 ；^ 不同-->1; >>  <<
+
+	n := 3
+	//n>>1
+	fmt.Println(n >> 1) //1
+	fmt.Println(n << 1) //6
+	x1 := byte(0)
+	x2 := byte(0)
+	fmt.Println(x1 & x2) //0
+	fmt.Println(x1 | x2) //0
+	fmt.Println(x1 ^ x2) //0
+
 }
